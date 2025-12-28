3a93ddcd3fcb18eda898995163836a5dbf9d8ca2
Wang Jian An <facebook0983481181@gmail.com>
2025-12-29 01:50:42 +0800

    資料下載功能

--- lccnet-competition/Controllers/CompetitionController.cs
+++ lccnet-competition/Controllers/CompetitionController.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles; // 用於取得 MIME Type
 
 namespace lccnet_competition.Controllers
 {
     public class CompetitionController : Controller
     {
+
+        private readonly IWebHostEnvironment _env;
+
+        public CompetitionController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +21,31 @@ namespace lccnet_competition.Controllers
         public IActionResult Data()
         {
             return View();
+        }
+
+        public IActionResult Download(string file)
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, "File", $"{file}.zip");
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound();
+            }
+
+            var contentType = GetContentType(filePath);
+
+            return PhysicalFile(filePath, contentType, $"{file}.zip");
+        }
+
+        // 輔助方法：自動判斷 MIME Type
+        private string GetContentType(string path)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(path, out string? contentType))
+            {
+                contentType = "application/octet-stream"; // 預設二進位流
+            }
+            return contentType;
         }
 
         public IActionResult Submission()
