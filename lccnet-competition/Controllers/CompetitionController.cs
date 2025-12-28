44c374a83586ae3a77481742c30bdcec52c40bbf
Wang Jian An <facebook0983481181@gmail.com>
2025-12-28 23:53:40 +0800

    建立競賽前端網頁

---
+++ lccnet-competition/Controllers/CompetitionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lccnet_competition.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Data()
+        {
+            return View();
+        }
+
+        public IActionResult Submission()
+        {
+            return View();
+        }
+
+        public IActionResult Leaderboard()
+        {
+            return View();
+        }
+    }
+}
