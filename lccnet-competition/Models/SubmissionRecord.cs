9db42a9be2f40de0cb8ac24afe63d88569d953a8
wangjianan <facebook0983481181@gmail.com>
2026-01-05 21:22:45 +0800

    加入任務別至模型中

--- lccnet-competition/Models/SubmissionRecord.cs
+++ lccnet-competition/Models/SubmissionRecord.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 
 namespace lccnet_competition.Models;
 
+public class ClassSubmissionFormat
+{
+    public string Id { get; set; } = null!;
+    public string Answer { get; set; } = null!;
+}
+
 public partial class SubmissionRecord
 {
     public int Id { get; set; }
 
     public int AccountId { get; set; }
 
-    public string FileName { get; set; } = null!;
+    public string? FileName { get; set; }
 
     public sbyte IsSuccess { get; set; }
 
@@ -18,4 +24,6 @@ public partial class SubmissionRecord
     public DateTime? CreateDatetime { get; set; }
 
     public DateTime? UpdateDatetime { get; set; }
+
+    public string Task { get; set; } = null!;
 }
