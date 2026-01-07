97780646850914161bfe892f2f02211c82f94b3d
wangjianan <facebook0983481181@gmail.com>
2026-01-07 20:13:04 +0800

    排行榜切換之 Model

---
+++ lccnet-competition/ViewModels/LeaderboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using lccnet_competition.Models;
+
+namespace lccnet_competition.ViewModels
+{
+    public class LeaderboardViewModel
+    {
+        public IEnumerable<Leaderboard>? ClassificationLeaderboard { get; set; }
+        public IEnumerable<Leaderboard>? SegmentationLeaderboard { get; set; }
+    }
+}
